bee79ebd77ad31c247fa1729210c983ebd7860ee
LoveKosmos <nataliiariabkova@gmail.com>
2018-09-06 23:15:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function takeANumber(line, curr){
+  line.push(curr);
+  for ( let i=0; i<=line.length; i++){
+     return (`Welcome,`+ ` ${curr}`+`. You are number` + ` ${line.length} `+`in line.`);
+  }
+}
+function nowServing(line){
+  for ( let i=0; i<=line.length; i++){
+     var next = line[i];
+     return (`Currently serving `+`${line[i]}`+ `.`)
+     line.shift[i];
+  }
+  if (line.length === 0){
+  return "There is nobody waiting to be served!"
+}
+}
