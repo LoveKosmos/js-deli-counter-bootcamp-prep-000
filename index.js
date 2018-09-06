4b90bcfeba234661b333a3444d770753b350ee66
LoveKosmos <nataliiariabkova@gmail.com>
2018-09-06 23:24:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function takeANumber(line, curr){
   }
 }
 function nowServing(line){
-  for ( let i=0; i<line.length; i=){
+  for ( let i=0; i<line.length;line.shift(i)){
      var next = (`Currently serving `+`${line[i]}`+ `.`)
-     line.shift[i];
+     i++
   }
   if (line.length === 0){
   next = "There is nobody waiting to be served!"
